bdb5c5981230f191745597eeddfe90ff5b2f549f
Sukhchain Singh <gillv625@gmail.com>
2018-10-11 15:20:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ var app = express()
 app.set('port', (process.env.PORT || 80))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hello World!');
+   (async () => {
+  puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  const page = await browser.newPage();
+  await page.goto('https://google.com');
+  await page.screenshot({path: 'example.png'});
+
+  await browser.close();
+})();
 })
 
 app.listen(app.get('port'), function() {

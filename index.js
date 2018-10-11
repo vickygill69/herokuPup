d738c1144e93bf1723c51bb10a1693aceac7aefa
Sukhchain Singh <gillv625@gmail.com>
2018-10-11 15:18:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
- 
-var express = require(‘express’);
-var port = process.env.PORT || 3000;
-var app = express();
-app.get(‘/’, function (req, res) {
- res.send(JSON.stringify({ Hello: ‘World’}));
- 
-});
-app.listen(port, function () {
- console.log(`Example app listening on port !`);
-});
+var express = require('express')
+var app = express()
 
+app.set('port', (process.env.PORT || 80))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})

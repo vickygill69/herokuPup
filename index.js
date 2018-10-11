f9e423fc5db5595907deb48e63bf20fd14d96670
Sukhchain Singh <gillv625@gmail.com>
2018-10-11 14:58:18 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer');
+
+(async () => {
+  puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  const page = await browser.newPage();
+  await page.goto('https://google.com');
+  await page.screenshot({path: 'example.png'});
+
+  await browser.close();
+})();
